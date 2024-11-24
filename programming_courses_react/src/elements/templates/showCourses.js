004f3450c64fe1cd132bbc4f1cf6c8cedd37ee7e
Pashos-2004 <Pashos-2004@yandex.ru>
2024-11-24 19:24:51 +0300

    Попытка автоматизации выгрузки контакотов 2

--- programming_courses_react/src/elements/templates/showCourses.js
+++ programming_courses_react/src/elements/templates/showCourses.js
@@ -18,7 +18,7 @@ export default  function ShowCourses(props){
     
 
     let buff = arr.map((el)=>{return <Course  courseText={el.get("courseText")} courseTitle={el.get("courseTitle")} picture={el.get("picture")} id={el.get("id")}   />})
-    
+
 
     return (
         <div>
