08febd4a8648a4042fe05f70a68a9d984fec68d2
Pashos-2004 <Pashos-2004@yandex.ru>
2024-12-02 15:19:30 +0300

    Начало разработки страниц для курсов

--- programming_courses_react/src/elements/templates/pageWithText.js
+++ programming_courses_react/src/elements/templates/pageWithText.js
@@ -1,16 +1,45 @@
 import React,{ useEffect, useState }  from 'react';
 import Logo from './logo_template.js';
+import { Box,colors,TextField } from '@mui/material';
 import PrevAndNextBnt from './PrevAndNextBnt.js'
 
-export default function PageWithText(){
-   
-    
-return (
-    <div>
-        <Logo/>
-        
+export default function PageWithText(props){
 
-    </div>);
+
+    const [PageTitle,SetPageTitle] = useState("C++ это круто");
+    const [Text,SetText] = useState(`jdfghbnjkdghkrdfg`)
+
+    function SplitIntoParagraphs(str,divider){
+        let res = str.split(divider).map(item => <p>{item}</p>)
+        return res
+    }
+
+    if(props.IsAdmin){
+        return (
+            <div>
+                <Logo/>
+                <Box paddingLeft={"20%"} paddingRight={"20%"}>
+
+                <h1>{PageTitle}</h1>
+                {SplitIntoParagraphs(Text,'\n')}
+                <PrevAndNextBnt/>
+
+                </Box>
+            </div>
+            );
+    }else{
+        return (
+            <div>
+                <Logo/>
+                <Box paddingLeft={"20%"} paddingRight={"20%"}  >
+                <h1>{PageTitle}</h1>
+                {SplitIntoParagraphs(Text,'\n')}
+                <PrevAndNextBnt/>
+                </Box>
+            </div>
+            );
+    }
+
 }
 
        
